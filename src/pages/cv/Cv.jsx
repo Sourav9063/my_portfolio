74bee1b9ff3b05caf3cddc92af53d586e2616b06
Sourav9063 <sourav.ahmed5654@gmail.com>
2026-05-20 16:29:45 +0600

    feat: two cv used

--- src/pages/cv/Cv.jsx
+++ src/pages/cv/Cv.jsx
@@ -15,20 +15,20 @@ export default function Cv() {
       ></iframe> */}
       <iframe
         className={`${style.cv}`}
-        src="https://sourav9063.github.io/CV"
+        src="https://sourav9063.github.io/CV/two"
         // type="application/pdf"
         title="CV"
       ></iframe>
       <div className={`${style.alternative}`}>
         <p>
           Alternative link to
-          <a target='_blank' rel='noreferrer' href="https://github.com/Sourav9063/CV/blob/main/index.pdf">
+          <a target='_blank' rel='noreferrer' href="https://github.com/Sourav9063/CV/blob/main/two/index.pdf">
             {" "}
             view the RESUME.
           </a>
         </p>
         <p>
-          <a href="assets/pdf/index.pdf" download="SouravAhmedResume.pdf">
+          <a href="https://raw.githubusercontent.com/Sourav9063/CV/refs/heads/main/two/index.pdf" download="SouravAhmedResume.pdf">
             Click here
           </a>{" "}
           to download the RESUME.
